62ce1b4e70c29d59de422d526114d2df2144b0fc
宋泽文 <songzewen@songzewendeMacBook-Pro.local>
2025-07-03 15:18:59 +0800

    add request

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRequest } from '../utils/request';
 
 function Login() {
+  const request = useRequest();
+
+  const login = async () => {
+    const email = "562172940@qq.com";
+    const password = "admin123";
+    const response = await request.post("auth/loginEmail", {
+      email,
+      password,
+    });
+    console.log(response);
+  }
+
+  useEffect(() => {
+    login();
+  }, []);
   return (
     <div className="login-container">
       <h2>Login page</h2>
