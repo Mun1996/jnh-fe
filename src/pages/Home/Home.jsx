f02f39f6719222bb74e68dc9c82dd1cf769d7cd7
unknown <2955857091@qq.com>
2025-07-08 09:56:15 +0800

    welcome page v2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,15 +10,23 @@ function Home() {
   };
 
   return (
-    <>
+    <div> 
+      <img className={styles.logo}src="src\assets\images\images\jnh-logo.png" alt="jnh-logo"></img>
+
       <h1 className={styles.title}>JOB NEAR HOME</h1>
-      <p className={styles.subtitle}>
-        WELCOME!
-      </p>
-      <button className={styles.loginButton} onClick={handleLoginClick}>
-        Login By Email
-      </button>
-    </>
+        <p className={styles.subtitle}>
+          WELCOME!
+        </p>
+      
+      <div className={styles.footerlogin}>
+        <button className={styles.loginButton} onClick={handleLoginClick}>Login By Email
+        </button>
+        <div className={styles.registerSign}>
+          <p>Not yet registered?</p>
+          <p className={styles.clickRegister}>Please register here</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
